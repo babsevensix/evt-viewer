05211cbe1df46da382b02c32998230071d7bbd59
Alberto Bellemo <alberto.bellemo@movesion.com>
2023-09-29 23:39:07 +0200

    WIP

--- src/app/panels/witness-panel/witness-panel.component.ts
+++ src/app/panels/witness-panel/witness-panel.component.ts
@@ -1,16 +1,110 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { EditionDataService } from '../../services/edition-data.service';
+import { distinctUntilChanged, first, map, startWith, switchMap, tap} from 'rxjs/operators';
+import { PrefatoryMatterParserService } from '../../services/xml-parsers/prefatory-matter-parser.service';
+import { EVTModelService } from '../../services/evt-model.service';
+import { BehaviorSubject, merge, Subject } from 'rxjs';
+import { EVTStatusService } from '../../services/evt-status.service';
+import { Page} from '../../models/evt-models';
+import { EntitiesSelectItem } from '../../components/entities-select/entities-select.component';
+import { EditionLevel, TextFlow } from '../../app.config';
 
 @Component({
   selector: 'evt-witness-panel',
   templateUrl: './witness-panel.component.html',
   styleUrls: ['./witness-panel.component.scss'],
+  providers:[
+    EditionDataService,
+    PrefatoryMatterParserService,
+    EVTModelService,
+    EVTStatusService
+  ]
+
 })
-export class WitnessPanelComponent {
+export class WitnessPanelComponent implements OnInit{
   @Input() witness: string;
   @Output() hide = new EventEmitter<boolean>();
+  updatePageFromScroll$ = new BehaviorSubject<void>(undefined);
+  updatePage$ = new BehaviorSubject<Page>(undefined);
+
+  public itemsToHighlight$ = new Subject<EntitiesSelectItem[]>();
+
+  editionLevel: EditionLevel= { 'id': 'diplomatic', 'label': 'diplomatic', 'enable': true };
+  private _tf: TextFlow;
+  public set textFlow(tf: TextFlow) {
+    this._tf = tf;
+  }
+  public get textFlow() {
+    return this._tf;
+  }
+
+  constructor(public editionDataService: EditionDataService, public evtModelService: EVTModelService,public evtStatus: EVTStatusService,) {
+    evtStatus.updateViewMode$.next({
+      icon:'', iconSet: undefined, id:'collation', label: 'prova', enable: true
+    });
+  }
 
   emitHide() {
     this.hide.emit(true);
   }
+
+  ngOnInit(): void {
+    this.editionDataService.editionUrlNumber$.next(1);
+
+    this.editionDataService.parsedEditionSource$.pipe(
+        first()
+    ).subscribe((res)=>{
+      console.log('res ', res);
+    });
+
+
+  }
+
+  public currentPage$ = merge(
+      this.updatePageFromScroll$.pipe(
+          startWith({}),
+          tap(()=>{
+            console.log('current page started');
+          }),
+          switchMap(()=> this.evtModelService.pages$
+              // , this.evtStatus.currentPage$
+          ),
+          tap(()=>{
+            console.log('with latest');
+          }),
+          map(( pages
+                 // , currentPage
+               ) => {
+            // if (this.mainContent && this.editionLevelID === 'critical') {
+            //   const mainContentEl: HTMLElement = this.mainContent.nativeElement;
+            //   const pbs = mainContentEl.querySelectorAll('evt-page');
+            //   let pbCount = 0;
+            //   let pbVisible = false;
+              //let pbId = '104v';
+            //   const docViewTop = mainContentEl.scrollTop;
+            //   const docViewBottom = docViewTop + mainContentEl.parentElement.clientHeight;
+            //   while (pbCount < pbs.length && !pbVisible) {
+            //     pbId = pbs[pbCount].getAttribute('data-id');
+            //     const pbElem = mainContentEl.querySelector<HTMLElement>(`evt-page[data-id="${pbId}"]`);
+            //     const pbRect = pbElem.getBoundingClientRect();
+            //     if (pbRect.top && (pbRect.top <= docViewBottom) && (pbRect.top >= docViewTop)) {
+            //       pbVisible = true;
+            //     } else {
+            //       pbCount++;
+            //     }
+            //   }
+            //   if (pbVisible && currentPage?.id !== pbId) {
+            //     this.updatingPageFromScroll = true;
+               //let currentPage = pages.find((p) => p.id === pbId);
+            //   }
+            // }
+            console.log('pages number', pages.length);
+            return pages[0];
+          }),
+      ),
+      this.updatePage$,
+  ).pipe(
+      distinctUntilChanged((x, y) => x?.id === y?.id),
+  );
 
 }
--- src/app/services/edition-data.service.ts
+++ src/app/services/edition-data.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { forkJoin, Observable, of } from 'rxjs';
-import { catchError, map, mergeMap, publishReplay, refCount, tap } from 'rxjs/operators';
+import { BehaviorSubject, forkJoin, Observable, of } from 'rxjs';
+import { catchError, map, mergeMap, publishReplay, refCount, switchMap, tap } from 'rxjs/operators';
 import { AppConfig } from '../app.config';
 import { OriginalEncodingNodeType } from '../models/evt-models';
 import { parseXml } from '../utils/xml-utils';
@@ -11,23 +11,39 @@ import { parseXml } from '../utils/xml-utils';
 })
 export class EditionDataService {
   private editionUrls = AppConfig.evtSettings.files.editionUrls || [];
-  public parsedEditionSource$: Observable<OriginalEncodingNodeType> = this.loadAndParseEditionData();
+  public parsedEditionSource$: Observable<OriginalEncodingNodeType>;
 
+  public editionUrlNumber$ = new BehaviorSubject<number>(0);
   constructor(
     private http: HttpClient,
   ) {
+      this.parsedEditionSource$ =  this.loadAndParseEditionData();
   }
 
   private loadAndParseEditionData() {
-    const editionUrl = this.editionUrls[0];
 
-    return this.http.get(editionUrl, { responseType: 'text' }).pipe(
-      map((source) => parseXml(source)),
-      mergeMap((editionData) => this.loadXIinclude(editionData, editionUrl.substring(0, editionUrl.lastIndexOf('/') + 1))),
-      publishReplay(1),
-      refCount(),
-      catchError(() => this.handleLoadingError()),
-    );
+      return this.editionUrlNumber$.pipe(
+          map((urlNumber) => this.editionUrls[urlNumber]),
+          tap((editionUrl)=>{
+             console.log('loading and parse edition', editionUrl);
+          }),
+          switchMap((editionUrl) =>{
+              return this.http.get(editionUrl, { responseType: 'text' }).pipe(map((xmlText)=>{
+                  return {
+                      editionUrl,
+                      source: xmlText
+                  };
+              }));
+          } ),
+            map((result) => {
+                return { editionData: parseXml(result.source), editionUrl: result.editionUrl}
+            }),
+            mergeMap((result) => this.loadXIinclude(result.editionData, result.editionUrl.substring(0, result.editionUrl.lastIndexOf('/') + 1))),
+            publishReplay(1),
+            refCount(),
+            catchError(() => this.handleLoadingError()),
+      );
+    
   }
 
   loadXIinclude(doc: HTMLElement, baseUrlPath: string) {
